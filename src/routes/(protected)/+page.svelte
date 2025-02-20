2b12ed4814678db6e479215e33abcea84fb80181
Julio Espinola <90486971+Julio9999@users.noreply.github.com>
2025-02-19 21:45:55 -0300

    inicializacion de shad-cn-svelte

--- src/routes/(protected)/+page.svelte
+++ src/routes/(protected)/+page.svelte
@@ -1,10 +1,36 @@
 <script lang="ts">
-  import { onMount } from "svelte";
+  import * as Table from "$lib/components/ui/table/index.js";
   import { UsersService } from "@services/users/users.service";
+  import { createFetcher } from "$lib/fetcher.svelte";
 
-    onMount(async() => {
-        const res = await UsersService.getUsers()
-    })
+    const {
+      getData,
+      isLoading
+    } = createFetcher({fetcher: () => UsersService.getUsers().then(res => res.data)})
+  
 </script>
 
-<p class="text-red-500 border-2 border-red-500"> Pagina principal</p>
+
+{#if isLoading()}
+  <p>Cargando</p>
+{:else if getData()}
+<Table.Root>
+  <Table.Header>
+    <Table.Row>
+      <Table.Head class="w-[100px]">Id</Table.Head>
+      <Table.Head>Nombre</Table.Head>
+      <Table.Head>Correo</Table.Head>
+    </Table.Row>
+  </Table.Header>
+  <Table.Body>
+    {#each getData() || [] as user, i (i)}
+      <Table.Row>
+        <Table.Cell>{user.id}</Table.Cell>
+        <Table.Cell>{user.name}</Table.Cell>
+        <Table.Cell>{user.email}</Table.Cell>
+      </Table.Row>
+    {/each}
+  </Table.Body>
+</Table.Root>
+{/if}
+
